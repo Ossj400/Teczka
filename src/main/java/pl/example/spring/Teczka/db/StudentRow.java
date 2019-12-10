23fb1775be7573c8c5bcff4b57181c91c5eca616
Ossj400 <Ossj400@gmail.com>
2019-12-11 00:05:44 +0100

    zad 9 dziala

--- src/main/java/pl/example/spring/Teczka/db/StudentRow.java
+++ src/main/java/pl/example/spring/Teczka/db/StudentRow.java
@@ -6,78 +6,86 @@ import pl.example.spring.Teczka.Student;
 import javax.persistence.*;
 import java.util.Set;
 
-
-
-import javax.persistence.*;
-import java.util.Set;
-
 @Entity
-//@Table(name="student_row")
-public class StudentRow {
+public class StudentRow
+{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private String number;
-    private String group1;
+    private String stud_group;
 
     public Set<ScoreRow> getScores() {
         return scores;
     }
-
     public void setScores(Set<ScoreRow> scores) {
         this.scores = scores;
     }
 
-    @OneToMany(mappedBy = "student")
-    private Set<ScoreRow> scores;
-
-    protected StudentRow() {
-    }
-
-    public StudentRow(String name, String number, String group1) {
-        this.name = name;
+    public StudentRow(String name, String number, String stud_group)
+    { this.name = name;
         this.number = number;
-        this.group1 = group1;
+        this.stud_group = stud_group;
     }
 
-    public Student toStudent() {
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getNumber()
+    {
+        return number;
+    }
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+
+    public String getStud_group()
+    {
+        return stud_group;
+    }
+    public void setGroup()
+    {
+        this.stud_group = stud_group;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+    public Long getId()
+    {
+        return id;
+    }
+
+    public Student toStudent(){
         return new Student(
                 this.getId(),
                 this.getName(),
                 this.getNumber(),
-                this.getGroup1());
+                this.getStud_group());
     }
 
-    public long getId() {
-        return id;
-    }
+
+
+
 
     public void setId(long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public void setStud_group(String stud_group) {
+        this.stud_group = stud_group;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @OneToMany(mappedBy = "student")
+    private Set<ScoreRow> scores;
 
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getGroup1() {
-        return group1;
-    }
-
-    public void setGroup1(String group1) {
-        this.group1 = group1;
-    }
 }
